538cb807cb694fea93c53e0dc22a3f311358109e
Marcus Göransson <>
2018-11-30 23:35:33 +0100

    Initial commit

---
+++ AdventOfCode2018/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventOfCode2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2018");
+            Console.WriteLine("===================");
+        }
+    }
+}
